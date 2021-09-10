e78001e758fc1dd537f9ff857b93595d13bc2f4c
Gggarin <nikitingggarin@gmail.com>
2021-09-10 14:29:21 +0300

    delete and edit

--- public/main.js
+++ public/main.js
@@ -1,28 +1,80 @@
 const table = document.getElementById('table');
+const editBtn = document.getElementById('editBtn');
+if (table) {
+  table.addEventListener('click', async (e) => {
+    if (e.target.dataset.id) {
+      e.preventDefault();
 
-table.addEventListener('click', async (e) => {
-  if (e.target.tagName === 'BUTTON') {
+      const orderId = e.target.dataset.id;
+
+      const response = await fetch('/', {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ orderId }),
+      });
+      // const email = await response.json();
+      // console.log(email);
+      if (response.ok) {
+        const tbody = e.target.closest('tbody');
+        const raw = e.target.closest(`#b${orderId}`);
+        const tableDiv = e.target.closest('.tableDiv');
+        // console.dir(tbody);
+        raw.remove();
+        if (tbody.innerText === '') {
+          tableDiv.innerHTML = '<H1 class="text-info">Пока нет доступных заказов</H1>';
+        }
+      // window.location.href = 'http://localhost:3000';
+      }
+    }
+    if (e.target.dataset.iddel) {
+      e.preventDefault();
+      const orderId = e.target.dataset.iddel;
+      console.log(orderId);
+      const response = await fetch('/', {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ orderId }),
+      });
+
+      if (response.ok) {
+        const tbody = e.target.closest('tbody');
+        const raw = e.target.closest(`#b${orderId}`);
+        const tableDiv = e.target.closest('.tableDiv');
+        // console.dir(tbody);
+        raw.remove();
+        if (tbody.innerText === '') {
+          tableDiv.innerHTML = '<H1 class="text-info">Пока нет доступных заказов</H1>';
+        }
+      }
+    }
+  // if (e.target.dataset.ided) {
+  //   // e.preventDefault();
+  //   console.log('hello');
+  //   // const response = await fetch('/edit');
+  // }
+  });
+}
+if (editBtn) {
+  editBtn.addEventListener('click', async (e) => {
     e.preventDefault();
 
-    const orderId = e.target.dataset.id;
+    const idOrder = e.target.dataset.id;
+    const editIdOrder = +idOrder.split('').slice(1).join('');
+    console.log(e.target.closest('#title'));
 
-    const response = await fetch('/', {
-      method: 'PATCH',
+    const title = document.getElementById('titleEdit').value;
+
+    const picture = document.getElementById('pictureEdit').value;
+    const original_price = document.getElementById('original_priceEdit').value;
+    const discount_price = document.getElementById('discount_priceEdit').value;
+
+    const response = await fetch('/:id/edit', {
+      method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ orderId }),
+      body: JSON.stringify({
+        editIdOrder, title, picture, original_price, discount_price,
+      }),
     });
-    // const email = await response.json();
-    // console.log(email);
-    if (response.ok) {
-      const tbody = e.target.closest('tbody');
-      const raw = e.target.closest(`#b${orderId}`);
-      const tableDiv = e.target.closest('.tableDiv');
-      // console.dir(tbody);
-      raw.remove();
-      if (tbody.innerText === '') {
-        tableDiv.innerHTML = '<H1 class="text-info">Пока нет доступных заказов</H1>';
-      }
-      // window.location.href = 'http://localhost:3000';
-    }
-  }
-});
+    window.location.href = 'http://localhost:3000';
+  });
+}
